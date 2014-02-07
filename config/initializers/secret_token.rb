9f07c330d013dc46c4e4731f8ddd54a4ae6fbabb
Ben Roesch <bcroesch@gmail.com>
2014-02-07 11:43:21 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Perky::Application.config.secret_key_base = '2c05e67330d224722978e629ed4ca32c9b6ae4ef4d3343d37cd4a189e03f03bd0f11a749cfb49ea80af0b7988c20ca4e9ffc71a48caab14e34642d8c90ab09cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Perky::Application.config.session_store :cookie_store, key: '_perky_session'

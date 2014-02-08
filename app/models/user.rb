bd694c642c28984633a4d1c5ccc775113acd0c64
Jack Schultz <jackschultz23@gmail.com>
2014-02-08 11:43:20 -0600

    acocunt view and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :account
+
 end

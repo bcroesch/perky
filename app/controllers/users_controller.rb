d59ed0c563832e91e51b9277096eefef3e84cadb
Jack Schultz <jackschultz23@gmail.com>
2014-02-08 12:36:04 -0600

    users templates and controller start

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,29 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    if current_user.admin
+      @user = current_user.account.users.find(params[:id])
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      flash[:alert] = 'Error with creation'
+      render action: 'new'
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:first_name ,:last_name, :email)
   end
 
 end

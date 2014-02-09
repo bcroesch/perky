70cf53fe246a7fcea0dd21ab776ab2c8a841bea9
Brandon Robins <brandon@ourlabel.com>
2014-02-08 18:54:20 -0600

    Remove @current_user from index

--- app/controllers/perks_controller.rb
+++ app/controllers/perks_controller.rb
@@ -5,7 +5,6 @@ class PerksController < ApplicationController
 
   # GET /perks
   def index
-    @current_user = User.first
     @perks = Perk.all 
     @prior_selections = current_user.perk_selections.to_json
   end
